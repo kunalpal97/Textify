1b6cc5ef81bd7ab98c9931e55211a4237b9ce02f
Kunal Pal <strive007boy@gmail.com>
2025-11-24 18:50:42 +0530

    Signup Controller and Functionality added

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,81 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullname, email, password } = req.body;
+
+  try {
+    if (!fullname || !email || !password) {
+      return res.staus(400).json({
+        message: "Plese Enter every Details Correctly ",
+      });
+    }
+
+    // check if password length is greater then 6 or not
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password Should be Atleast 6 character long",
+      });
+    }
+
+    // check if email valid or not we will use regx
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "Please Enter valid Email",
+      });
+    }
+
+    // yaha pe humko pta rehna chahiye ki kon se user ne wo email le liya taki duplicate email na aayege
+    // using model we can have a track of that okay
+    const user = await User.findOne({ email });
+    if (user)
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+
+    // and for password hashing we will use the bcrypt so that password will be safe and secure here
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+    
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+
+     // todo: send a welcome email to user
+    } else {
+      res.status(400).json({
+        message: "Invalid user data",
+      });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error);
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js'
+import connectDB from './lib/db.js';
 
 dotenv.config();
 
@@ -10,10 +11,13 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.json()); // req.body mai..jo bhajege wo aayega idhar pe 
+
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageRoutes);
 
 
 app.listen(PORT , () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    connectDB();
 })
