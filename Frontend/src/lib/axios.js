4061af14354827eeb62fa1e2efed6e4bcb0cd206
Kunal Pal <strive007boy@gmail.com>
2025-12-06 16:16:10 +0530

    changes in the code Base url added with deployed backend

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
-
-import axios from "axios";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 export const axiosInstance = axios.create({
-
-    baseURL : import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-    withCredentials : true,
-
-})
+  baseURL: `${BASE_URL}/api`,
+  withCredentials: true,
+});
