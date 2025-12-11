6be1a1ce0bacbd8a627fa6841d3f0fb727cdc675
Kunal Pal <strive007boy@gmail.com>
2025-12-11 21:16:10 +0530

    Chatting page section work is going on header completed

--- Frontend/src/components/ChatConatiner.jsx
+++ Frontend/src/components/ChatConatiner.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore.js'
+import { useAuthStore } from '../store/useAuthStore.js';
+import ChatHeader from './ChatHeader.jsx';
 
 function ChatConatiner() {
+
+  const {selectedUser , messages , getMessagesByUserId , isMessagesLoading} = useChatStore();
+  
+  const {authUser} = useAuthStore();
+
+  useEffect(() => {
+    getMessagesByUserId(selectedUser._id);
+  }, [selectedUser , getMessagesByUserId]) // this is dependency array here 
+
+
   return (
-    <div>ChatConatiner</div>
+    <>
+    <ChatHeader/>
+    </>
   )
 }
 
